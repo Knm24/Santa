1c14649420ad0c7f00b00697cdf9037ff7bc1a41
Knm24 <143102406+Knm24@users.noreply.github.com>
2024-04-06 01:22:32 +0500

    add: button need custom
    https://mui.com/material-ui/react-button/

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,27 @@
 import styles from './page.module.css'
 import Switch from "@mui/material/Switch";
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
 
 const label = { inputProps: { "aria-label": "Switch demo" } };
 
 export default function Home() {
- return (
-   <div className={styles.container}>
-     <div>
-       <span>With default Theme:</span>
-     </div>
-     <Switch {...label} defaultChecked />
-     <Switch {...label} />
-     <Switch {...label} disabled defaultChecked />
-   </div>
- );
+  return (
+    <div className={styles.container}>
+      <div>
+        <span>Santa test:</span>
+      </div>
+      <Switch {...label} defaultChecked />
+      <Switch {...label} />
+      <Switch {...label} disabled defaultChecked />
+
+
+      <Stack spacing={2} direction="row">
+        <Button variant="text">Text</Button>
+        <Button variant="contained">Contained</Button>
+        <Button variant="outlined">Outlined</Button>
+      </Stack>
+
+    </div>
+  );
 }
